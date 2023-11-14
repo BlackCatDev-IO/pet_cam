0bad404f8ad75abd8f6426ec95339245328c9445
Loren Aguey <loren@blackcatdev.io>
2023-11-14 00:31:29 -0500

    initial commit

---
+++ android/app/src/main/kotlin/com/blackcatdev/org/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.blackcatdev.org.pet_cam
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:pet_cam/app/app.dart';
+import 'package:pet_cam/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
