7caee1e9117a3c1f1294e09f931f0708a7d2690d
Loren Aguey <loren@blackcatdev.io>
2023-11-14 01:37:08 -0500

    Add BlocProvider<SocketBloc> and init listeners

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,22 +1,30 @@
 import 'package:flutter/material.dart';
-import 'package:pet_cam/counter/counter.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pet_cam/l10n/l10n.dart';
+import 'package:pet_cam/sockets/bloc/socket_bloc.dart';
+import 'package:pet_cam/sockets/socket_repository.dart';
+import 'package:pet_cam/sockets/view/socket_page.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        appBarTheme: const AppBarTheme(color: Color(0xFF13B9FF)),
-        colorScheme: ColorScheme.fromSwatch(
-          accentColor: const Color(0xFF13B9FF),
+    return BlocProvider<SocketBloc>(
+      create: (context) => SocketBloc(
+        socketRepository: SocketRepository(),
+      )..add(SocketInitListener()),
+      child: MaterialApp(
+        theme: ThemeData(
+          appBarTheme: const AppBarTheme(color: Color(0xFF13B9FF)),
+          colorScheme: ColorScheme.fromSwatch(
+            accentColor: const Color(0xFF13B9FF),
+          ),
         ),
+        localizationsDelegates: AppLocalizations.localizationsDelegates,
+        supportedLocales: AppLocalizations.supportedLocales,
+        home: SocketPage(),
       ),
-      localizationsDelegates: AppLocalizations.localizationsDelegates,
-      supportedLocales: AppLocalizations.supportedLocales,
-      home: const CounterPage(),
     );
   }
 }
